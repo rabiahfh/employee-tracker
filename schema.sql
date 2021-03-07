a0d7d43d8a47556d778bc513bbe7a507ebb12ad5
Rabiah Hogans <hogansrabiah45@gmail.com>
2021-03-07 14:32:16 -0500

    feat: added role questions

--- schema.sql
+++ schema.sql
@@ -27,12 +27,37 @@ name VARCHAR(30) NOT NULL,
 );
 
 INSERT INTO department (id, name)
-VALUES (1, "rabiah");
+VALUES (1, "Rabiah");
+INSERT INTO department (id, name)
+VALUES (2, "Tammy");
+INSERT INTO department (id, name)
+VALUES (3, "Edna");
+INSERT INTO department (id, name)
+VALUES (4, "Irv");
+INSERT INTO department (id, name)
+VALUES (5, "Diane");
+
 INSERT INTO role (id, title, salary, department_id )
-VALUES (2, " manager",50000.00, 2);
+VALUES (1, " manager",50000.00, 1);
+INSERT INTO role (id, title, salary, department_id )
+VALUES (2, " employee", 40000.00, 2);
+INSERT INTO role (id, title, salary, department_id )
+VALUES (3, " inturn",30000.00, 3);
+INSERT INTO role (id, title, salary, department_id )
+VALUES (4, " engineer",20000.00, 4);
+INSERT INTO role (id, title, salary, department_id )
+VALUES (5, " student",10000.00, 5);
 
 INSERT INTO employee (id, first_name, last_name,
   role_id,  manager_id)
-VALUES (3,"Ahmed", "Rockington", 2, 7);
-
+VALUES (1,"Rabiah", "Hogans", 1, 1);
+INSERT INTO employee (id, first_name, last_name,
+  role_id,  manager_id)
+VALUES (2,"Tammy", "Clark", 2, 2);INSERT INTO employee (id, first_name, last_name,
+  role_id,  manager_id)
+VALUES (3,"Edna", "Johnson", 3, 3);INSERT INTO employee (id, first_name, last_name,
+  role_id,  manager_id)
+VALUES (4,"Irv", "Rockington", 4, 4);INSERT INTO employee (id, first_name, last_name,
+  role_id,  manager_id)
+VALUES (5,"Diane", "White", 5, 5);
 
