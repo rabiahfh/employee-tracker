4d0d5c54dd7fd05b2729733e76a2ada0f213d2f5
Rabiah Hogans <hogansrabiah45@gmail.com>
2021-03-07 16:25:05 -0500

    Finished add employee functionality

--- employeetracker.js
+++ employeetracker.js
@@ -58,9 +58,8 @@ const askFirstQuestions = () => {
                                 })
 
                         } else if (choice == 'Role') {
-                            
+
                             connection.query('SELECT * FROM department', function (error, results, fields) {
-    
                                 const departments = [];
                                 for (let i = 0; i < results.length; i++) {
                                     departments.push(results[i].name);
@@ -83,28 +82,88 @@ const askFirstQuestions = () => {
                                         message: "Select the department you belong to ",
                                         name: "department_name",
                                         choices: departments
-                                    
+
                                     }
                                 ])
-                                .then(answers => {
-                                    const {role_name, role_salary, department_name} = answers;
-                                    const department = results.find(department => {
-                                        return department.name = department_name;
+                                    .then(answers => {
+                                        const { role_name, role_salary, department_name } = answers;
+                                        const department = results.find(department => {
+                                            return department.name == department_name;
+                                        })
+                                        const departmentId = department.id;
+                                        connection.query('INSERT INTO role (title, salary, department_id) VALUES(?,?,?)', [role_name, role_salary, departmentId], function (error, results, fields) {
+                                            console.log('Role has been added.');
+                                            askFirstQuestions();
+                                        })
                                     })
-                                    const departmentId = department.id;
-                                    connection.query('INSERT INTO role (title, salary, department_id) VALUES(?,?,?)', [role_name, role_salary, departmentId], function (error, results, fields) {
-                                        console.log('Role has been added.');
-                                        askFirstQuestions();
-                                    })
-                                })
                             })
                         } else {
-                            
+                            connection.query('SELECT * FROM role', function (error, role_results, fields) {
+                                const roles = [];
+                                for (let i = 0; i < role_results.length; i++) {
+                                    roles.push(role_results[i].title);
+                                }
+                                connection.query('SELECT * FROM employee', function (error, employee_results, fields) {
+                                    const employees = [];
+                                    for (let i = 0; i < employee_results.length; i++) {
+                                        employees.push(employee_results[i].first_name + ' ' + employee_results[i].last_name);
+                                    }
+                                    employees.push('None');
+                                    inquirer.prompt([
+                                        {
+                                            type: "input",
+                                            message: "Enter your first name: ",
+                                            name: "first_name",
+                                        },
+                                        {
+                                            type: "input",
+                                            message: "Enter your last name: ",
+                                            name: "last_name",
+                                        },
+                                        {
+                                            type: "list",
+                                            message: "Select your role: ",
+                                            name: "employee_role",
+                                            choices: roles
+                                        },
+                                        {
+                                            type: "list",
+                                            message: "Select your manager: ",
+                                            name: "employee_manager",
+                                            choices: employees
+                                        },
+                                    ])
+                                        .then(answers => {
+                                            const { first_name, last_name, employee_role, employee_manager } = answers;
+                                            const role = role_results.find(role => {
+                                                return role.title === employee_role;
+                                            })
+                                            const roleId = role.id;
+                                            const manager = employee_results.find(employee => {
+                                                return employee.first_name + ' ' + employee.last_name === employee_manager;
+                                            })
+                                            let managerId = '';
+                                            if (manager != null) {
+                                                managerId = manager.id;
+                                            }
+                                          
+                                            connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)', [first_name, last_name, roleId, managerId ], function (error, results, fields) {
+                                                console.log('Employee has been added.');
+                                                askFirstQuestions();
+                                            })
+                                        })
+                                })
+
+
+
+
+                            });
+
 
                         }
                     })
             }
-                            // QUestion to ask which table they want to view
+            // QUestion to ask which table they want to view
 
             else if (choice === "VIEW DEPARTMENTS, ROLES, OR EMPLOYEES") {
                 inquirer.prompt([
@@ -156,12 +215,12 @@ const askFirstQuestions = () => {
                     },
                     {
                         type: "input",
-                        message: "What is your new role id:",      
+                        message: "What is your new role id:",
                         name: "roleId"
                     },
-                ]).then(answers=>{
-                    const {employeeId, roleId}= answers
-                    connection.query("UPDATE employee set role_id = ? WHERE id = ?", [roleId, employeeId],function(err, results){
+                ]).then(answers => {
+                    const { employeeId, roleId } = answers
+                    connection.query("UPDATE employee set role_id = ? WHERE id = ?", [roleId, employeeId], function (err, results) {
                         console.log("employees role has been updated")
                         askFirstQuestions()
                     })
